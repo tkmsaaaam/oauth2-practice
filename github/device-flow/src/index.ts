51cf1ed2b31dbc3f0dc0e5319a3c3aed70ca4430
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-01-17 22:31:20 +0900

    add device-flow

---
+++ github/device-flow/src/index.ts
@@ -0,0 +1,75 @@
+import fetch from 'node-fetch';
+require('dotenv').config();
+
+const sleep = (time: number) =>
+	new Promise(resolve => setTimeout(resolve, time * 1000));
+
+type DeviceResponse = {
+	device_code: string;
+	user_code: string;
+	verification_url: string;
+	expires_in: number;
+	interval: number;
+};
+
+type OauthResponse = {
+	access_token: string;
+	token_type: string;
+	scope: string;
+};
+
+(async () => {
+	const clientId = process.env.CLIENT_ID;
+	const deviceCode: string = await fetch(
+		`https://github.com/login/device/code?client_id=${clientId}&scope=`,
+		{
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+			},
+		}
+	)
+		.then(res => {
+			return res.json();
+		})
+		.then(data => {
+			console.log('request to login/device/code');
+			console.log(data);
+			return (data as DeviceResponse).device_code;
+		});
+
+	await sleep(60);
+
+	const accessToken: string = await fetch(
+		`https://github.com/login/oauth/access_token?client_id=${clientId}&device_code=${deviceCode}&grant_type=urn:ietf:params:oauth:grant-type:device_code`,
+		{
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+			},
+		}
+	)
+		.then(res => {
+			return res.json();
+		})
+		.then(data => {
+			console.log('request to login/oauth/access_token');
+			console.log(data);
+			return (data as OauthResponse).access_token;
+		});
+
+	fetch('https://api.github.com/user', {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${accessToken}`,
+		},
+	})
+		.then(res => {
+			return res.json();
+		})
+		.then(data => {
+			console.log('request to /user');
+			console.log(data);
+		});
+})();
