2b70ad93e2d354d7078aa93fde25904831fa49d5
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-01-24 20:03:02 +0900

    add Promise<void>

--- github/device-flow/src/index.ts
+++ github/device-flow/src/index.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const sleep = (time: number) =>
+const sleep = (time: number): Promise<void> =>
 	new Promise(resolve => setTimeout(resolve, time * 1000));
 
 type DeviceResponse = {
